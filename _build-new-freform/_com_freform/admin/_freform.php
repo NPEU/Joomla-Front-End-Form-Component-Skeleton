9297c99a7f686e29cbf6f0732cd06f8d67ea370f
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-01-17 10:38:08 +0000

    Added forced language load

--- _build-new-freform/_com_freform/admin/_freform.php
+++ _build-new-freform/_com_freform/admin/_freform.php
@@ -9,6 +9,15 @@
 
 defined('_JEXEC') or die;
 
+// Force-load the Admin language file to avoid repeating form language strings:
+// (this model is used in the front-end too, and the Admin lang isn't auto-loaded there.)
+$lang = JFactory::getLanguage();
+$extension = 'com__freform';
+$base_dir = JPATH_COMPONENT_ADMINISTRATOR;
+$language_tag = 'en-GB';
+$reload = true;
+$lang->load($extension, $base_dir, $language_tag, $reload);
+
 // Set some global property
 #$document = JFactory::getDocument();
 #$document->addStyleDeclaration('.icon-helloworld {background-image: url(../media/com_helloworld/images/tux-16x16.png);}');
